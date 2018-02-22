d69951d27701d54bae529fd17f1080a3cab3ae3d
Austin Rossow <arossow@ucsd.edu>
2018-02-22 11:44:36 -0800

    Add calorie data scraping.

--- app.js
+++ app.js
@@ -43,131 +43,6 @@ var restaurants = [
     oceanViewTerrace
 ];
 
-/**
- * Main scraper page and function. This function iterates through the hdh restaurant sites and gathers menu items.
- */
-app.get('/scrape', function(req, res){
-
-    //bigObj is the array that will hold food data in JSON file.
-    var bigObj = {table : []};
-
-    //This loop iterates through restaurants and adds each food item on the menu.
-    for(i = 0; i <= 5; i++) {
-
-        //Set url to restaurant in restaurant array.
-        var url = restaurants[i];
-        request(url, function (error, response, html) {
-            if (!error) {
-
-                //Use cheerio to load page
-                var $ = cheerio.load(html);
-                //Select element with id of location (e.g. Pines, Foodworx, etc.)
-                var loc = $('#HoursLocations_locationName').text();
-
-                //For each list item in unorder menu list, get item string with name and price/
-                $('ul[class="itemList"] > li > a[href]').each(function (i, e) {
-
-                    //unorganized Item Name ($price).
-                    var data = $(this).text();
-
-                    //split item by parenthesis and $.
-                    var itemDetails = data.toString().split(/[()$]+/)
-                    var name = itemDetails[0];
-                    var price = itemDetails[1];
-
-                    //Get this <a>'s href and store in link.
-                    //TODO: fix href
-                    var link = $(this).attr('href');
-
-                    //get the next element after <a> which is <img> and get alt description (Vegetarian, Vegan, etc.)
-                    var veg = $(this).next().attr('alt');
-
-                    //Construct a full URL from the relative link gather from <a>.
-                    var fullLink = "https://hdh.ucsd.edu/DiningMenus/" + link.toString();
-
-                    //Create a JS object with the scraped values.
-                    var myObj = {
-                        name: name,
-                        loc: loc,
-                        nutLink: fullLink,
-                        price: price,
-                        diet: veg,
-                        cal: ""
-                    };
-
-                    //add the JS object to the table
-                    bigObj.table.push(myObj);
-
-                    //Print object values to console
-                    //console.log("name: " + name + "\n      price: " + price + "\n      link:  " + fullLink + "\n      loc:   " + loc + "\n      alt:   " + veg + "\n");
-                    //console.log("table size: " + bigObj.table.length);
-
-                    //write table to json file.
-                    fs.writeFile('final2.json', JSON.stringify(bigObj, null, 4), function(err){
-                       // console.log(name + " was added to fuck.json");
-                    })
-
-
-
-                })
-
-            }
-        })
-
-    }
-    //Output to browser
-    res.send('Check the console.')
-});
-
-/**
- * Testing iteration through the json file
- */
-app.get('/iterate', function(req, res) {
-    var file = require("./final2.json");
-    var obj = file.table;
-    for(var ind in obj){
-        console.log( "name: " + obj[ind].name);
-    }
-
-    res.send("lol.")
-
-});
-
-/**
- * Scraper that goes through each json objects full link and scrapes calorie data.
- * TODO: fix selector to get calorie data, verify link is correct.
- */
-app.get('/calories', function(req, res) {
-
-    //Get food array from json file
-    var file = require("./final2.json");
-    var obj = file.table;
-
-    //For each food item in json file.
-    for (var ind in obj) {
-
-        //get the link from the object
-        var myObj = obj[ind];
-        var url = obj[ind].nutLink;
-        console.log("url: " + url);
-
-        //From the url...
-        request(url, function (error, response, html) {
-            if (!error) {
-                var $ = cheerio.load(html);
-
-                var calories = $('#lblItemHeader').text();
-
-                myObj.cal = calories;
-
-                console.log("name: " + myObj.name + "cals: " + calories);
-
-            }
-        })
-    }
-
-    res.send("check log pls.");
-});
 
 /**
  * Converting the json file to sqlite table.
@@ -179,7 +54,7 @@ app.get('/convert', function(req, res) {
     var db = new sqlite3.Database('data.db');
 
 
-    var json = require("./final2.json");
+    var json = require("./FoodsWithCalories.json");
     var foods = json.table;
     db.serialize(function() {
         db.run("CREATE TABLE items (item TEXT, location TEXT, calories INTEGER, price REAL, dietary TEXT, nutLink TEXT)");
@@ -187,6 +62,7 @@ app.get('/convert', function(req, res) {
         var stmt = db.prepare("INSERT INTO items VALUES (?,?,?,?,?,?)");
 
         for(var ind in foods){
+            console.log("ind: " + ind);
             var n = foods[ind].name;
             var l = foods[ind].loc;
             var c = foods[ind].cal;
@@ -198,9 +74,6 @@ app.get('/convert', function(req, res) {
         }
         stmt.finalize();
 
-        db.each("SELECT name, loc FROM items", function(err, row) {
-            //console.log("food: "+ row.name + "|| loc: " + row.location);
-        });
     });
 
     db.close();
@@ -208,6 +81,65 @@ app.get('/convert', function(req, res) {
     res.send('Check your console.')
 
 });
+
+app.get('/scraper', function(req,res) {
+
+    var bigTableObject = {table : []};
+
+    for(i = 0; i < restaurants.length; ++i){
+        var URL = restaurants[i];
+        request(URL, function (error, response, html) {
+            if (!error) {
+                //Use cheerio to load page
+                var $ = cheerio.load(html);
+                //Select element with id of location (e.g. Pines, Foodworx, etc.)
+                var loc = $('#HoursLocations_locationName').text();
+
+                //For each list item in unorder menu list, get item string with name and price/
+                $('ul[class="itemList"] > li > a[href]').each(function (i, e) {
+
+                    //unorganized Item Name ($price).
+                    var data = $(this).text();
+                    //split item by parenthesis and $.
+                    var itemDetails = data.toString().split(/[()$]+/);
+                    var name = itemDetails[0];
+                    var price = itemDetails[1];
+                    var link = $(this).attr('href');
+                    //get the next element after <a> which is <img> and get alt description (Vegetarian, Vegan, etc.)
+                    var veg = $(this).next().attr('alt');
+                    //Construct a full URL from the relative link gather from <a>.
+                    var fullLink = "https://hdh.ucsd.edu/DiningMenus/" + link.toString();
+                    console.log(loc + ", " + name + ", " + price + ", " + veg + ", " + fullLink);
+
+                    request(fullLink, function (error2, response2, html2) {
+                        if(!error2) {
+                            var cal$ = cheerio.load(html2);
+                            var calories = cal$('table #tblFacts span').text().toString().split(" ");
+                            var calData = calories[1];
+                            console.log(name + ": " + calories + "Kcal for " + price + "$ at " + loc + " added to menu.");
+                            var myObj = {
+                                name: name,
+                                loc: loc,
+                                nutLink: fullLink,
+                                price: price,
+                                diet: veg,
+                                cal: calData
+                            };
+                            bigTableObject.table.push(myObj);
+                            fs.writeFile('FoodsWithCalories.json', JSON.stringify(bigTableObject, null, 4), function(err){
+                                console.log("FoodsWithCalories.json was created successfully.");
+                            })
+                        }
+                    })
+                })
+            }
+        })
+    }
+    res.send('Check the console.');
+});
+
+
+
 
 app.listen('8081')
 
